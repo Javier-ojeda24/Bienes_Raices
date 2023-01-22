ed33411c98a980e2d1bf5591e44cf0a4bd6f53f8
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2023-01-22 12:31:32 -0300

    css

--- src/components/Navegacion.jsx
+++ src/components/Navegacion.jsx
@@ -3,10 +3,10 @@ import { NavLink } from "react-router-dom";
 
 export const Navegacion = () => {
   return (
-    <>
+    <div class="bg-blue-400 w-3">
       <NavLink to="/inicio">Inicio</NavLink>
       <NavLink to="/sobre-nosotros">Sobre Nosotros</NavLink>
       <NavLink to="/contacto">Contacto</NavLink>
-    </>
+    </div>
   );
 };
