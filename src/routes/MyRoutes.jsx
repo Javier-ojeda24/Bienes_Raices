f46f4fd7a7bf7cc2820de692217499dc0a5e24dd
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2023-01-22 12:11:29 -0300

    rutas

--- src/routes/MyRoutes.jsx
+++ src/routes/MyRoutes.jsx
@@ -4,8 +4,17 @@ import { Inicio } from "../components/Inicio";
 export const MyRoutes = () => {
   return (
     <BrowserRouter>
+      <NavLink>
+        <ul>
+          <li to="/inicio">Inicio</li>
+          <li to="/sobre-nosotros">Sobre Nosotros</li>
+          <li to="/contacto">Contacto</li>
+        </ul>
+      </NavLink>
       <Routes>
         <Route path="/" element={<Inicio />} />
+        <Route path="/inicio" element={<Inicio />} />
+       
       </Routes>
     </BrowserRouter>
   );
