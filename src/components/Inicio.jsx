254777d86d28804a7697a79d31375fa32e6f5848
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2023-01-25 18:12:51 -0300

    video sacado

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 export const Inicio = () => {
   return (
     <div>
-      <h1>Holaa</h1>
+      <h1>
+        ¿Estás buscando un lugar para construir tu futuro? ¡Compra un terreno
+        con nosotros hoy!
+      </h1>
+      <button>
+        <Link to="/lotes">Aca puedes ver</Link>{" "}
+      </button>
+      
     </div>
   );
 };
