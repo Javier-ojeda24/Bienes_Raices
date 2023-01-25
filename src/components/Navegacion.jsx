62499a130fb9d94f2f246cb59bf3d0df23acf83c
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2023-01-25 01:05:17 -0300

    css

--- src/components/Navegacion.jsx
+++ src/components/Navegacion.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import "./nav.css";
+import logo from "../assets/logo.png";
 
 export const Navegacion = () => {
   const [menuToggle, setMenuToggle] = useState(false);
@@ -12,31 +13,42 @@ export const Navegacion = () => {
   const handleToggle = () => {
     if (menuToggle) {
       setMenuToggle(false);
-      // refMenu.current.style.top = "-100%";
-      // refIconToggle.current.classList.replace();
+      refMenu.current.style.top = "-100%";
+      refIcon.current.classList.replace("bi-x-lg", "bi-list");
     } else {
       setMenuToggle(true);
-      const calculate = refNav.current.offsetTop + refNav.current.clientHeight;
+      const calculate =
+        refNav.current.offsetTop + refNav.current.clientHeight - 1;
+      refMenu.current.style.top = calculate + "px";
+      refIcon.current.classList.replace("bi-list", "bi-x-lg");
     }
   };
 
   return (
-    <nav ref={refNav} className="nav">
-      <button onClick={handleToggle} ref={refBtnToggle} className="btnToggle">
-        <i ref={refIcon} className="bi bi-list fs-1"></i>
-      </button>
+    <div className="navContainer">
+      <nav ref={refNav} className="nav">
+        <button onClick={handleToggle} ref={refBtnToggle} className="btnToggle">
+          <i ref={refIcon} className="bi bi-list fs-1"></i>
+        </button>
 
-      <ul className="ul" ref={refMenu}>
-        <li>
-          <Link to="/inicio">Inicio</Link>
-        </li>
-        <li>
-          <Link to="/sobre-nosotros">Sobre Nosotros</Link>
-        </li>
-        <li>
-          <Link to="/contacto">Contacto</Link>
-        </li>
-      </ul>
-    </nav>
+        <ul className="ul" ref={refMenu}>
+          <li>
+            <a href="#">
+              {" "}
+              <img className="logoImg"  src={logo} alt="logo" />
+            </a>
+          </li>
+          <li>
+            <Link to="/inicio">Inicio</Link>
+          </li>
+          <li>
+            <Link to="/sobre-nosotros">Sobre Nosotros</Link>
+          </li>
+          <li>
+            <Link to="/contacto">Contacto</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
