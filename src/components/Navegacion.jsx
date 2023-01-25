45204844526a08800b58261fff32bf9b64c6d1cc
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2023-01-24 23:41:20 -0300

    css navegacion

--- src/components/Navegacion.jsx
+++ src/components/Navegacion.jsx
@@ -1,12 +1,42 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState, useRef } from "react";
+import { Link } from "react-router-dom";
+import "./nav.css";
 
 export const Navegacion = () => {
+  const [menuToggle, setMenuToggle] = useState(false);
+  const refNav = useRef();
+  const refMenu = useRef();
+  const refBtnToggle = useRef();
+  const refIcon = useRef();
+
+  const handleToggle = () => {
+    if (menuToggle) {
+      setMenuToggle(false);
+      // refMenu.current.style.top = "-100%";
+      // refIconToggle.current.classList.replace();
+    } else {
+      setMenuToggle(true);
+      const calculate = refNav.current.offsetTop + refNav.current.clientHeight;
+    }
+  };
+
   return (
-    <div className="flex items-center justify-center flex-wrap bg-teal-500 p-6">
-      <NavLink to="/inicio">Inicio</NavLink>
-      <NavLink to="/sobre-nosotros">Sobre Nosotros</NavLink>
-      <NavLink to="/contacto">Contacto</NavLink>
-    </div>
+    <nav ref={refNav} className="nav">
+      <button onClick={handleToggle} ref={refBtnToggle} className="btnToggle">
+        <i ref={refIcon} className="bi bi-list fs-1"></i>
+      </button>
+
+      <ul className="ul" ref={refMenu}>
+        <li>
+          <Link to="/inicio">Inicio</Link>
+        </li>
+        <li>
+          <Link to="/sobre-nosotros">Sobre Nosotros</Link>
+        </li>
+        <li>
+          <Link to="/contacto">Contacto</Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
